846a6d4c59e05f3be73fc3c9ace907c7a44d8279
ged <gedw99@gmail.com>
2019-08-30 16:38:32 +0200

    added go-flutter desktop

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/apple/fvm/master"
+export "FLUTTER_APPLICATION_PATH=/Users/apple/workspace/go/src/github.com/joeblew99/zoom-widget/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/apple/fvm/master/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
---
+++ example/lib/app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_widget/zoom_widget.dart';
+ 
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Zoom example'),
+        ),
+        body: Zoom(
+          width: 1800,
+          height: 1800,
+          initZoom: 0.0,
+          child: Center(
+            child: Container(
+              child: Text('Hello World'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
